9cb1fd1d9e7e3abbdcfca6b7d21187a0450e4d6c
Gabrielle Cole <gcole60302@gmail.com>
2015-04-11 23:43:47 -0500

    Final Stuff
    just about done with HW1 subfile, just some final touches needed before
    submission

--- HW1/HW1_d.py
+++ HW1/HW1_d.py
@@ -3,26 +3,30 @@ import matplotlib.pyplot as plt
 mean = np.zeros(3)
 index = np.arange(3)
 for i in range(len(index)):
-    s = np.random.poisson(3,10)
+    s = np.random.poisson(3,1000) # so 1000 is our N, the larger it gets the smaller our final value becomes
     mean[i] = np.mean(s)
 
 
 a = np.mean(mean)
 a_v = np.var(mean)
+d = np.std(s)
+
+    
 print a, a_v
 print s
-d = np.std(s)
+
 print d
 s[s<(d*3)] = 0
 print s
-c = s.sort()
-print c
-b = np.trim_zeros(c)
+s.sort()
+print s
+print len(s)
+b = np.trim_zeros(s)
 print b
 print len(b)
 
-
-
+print len(b)/float(len(s))
+#as N gets larger this value the measure of non-G gets smaller
 
         
   
